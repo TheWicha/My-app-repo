aaf64372b2d1bcc1bb6e18c0f4f755fe247f9b1e
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-01-29 21:28:35 +0100

    Introduce Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Navbar extends React.Component {
+    render() {
+      return <div className='navbar'>{this.props.children}</div>
+    }
+  }
+ 
+export default Navbar;
