c842ed069fe357274cb0358544cf7d1129f370bb
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-01-29 21:27:34 +0100

    Introduce NavItem component

---
+++ src/components/NavItem/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class NavItem extends React.Component {
+    render() {
+      return <div className='nav-item'>{this.props.children}</div>
+    }
+  }
+ 
+export default NavItem;
