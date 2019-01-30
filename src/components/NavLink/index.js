f207ab7791e6b960dd6ecf0e52d6bc5b338171ae
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-01-30 09:43:39 +0100

    Introduce new NavLogo structure

--- src/components/NavLink/index.js
+++ src/components/NavLink/index.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 
 class NavLink extends React.Component {
     render() {
-      return <div className='nav-link'>{this.props.children}</div>
+      return <div className='nav-link'>
+        <a href={this.props.href}> {this.props.children} </a>
+      </div>
     }
   }
  
