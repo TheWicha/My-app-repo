a733fd35b48d84361985290082bcf22f8b8dcdf1
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-01 19:18:48 +0100

    Add search form

--- src/components/InnerHeader/index.js
+++ src/components/InnerHeader/index.js
@@ -4,11 +4,19 @@ import React, { Component } from 'react';
 class InnerHeader extends React.Component {
     render() {
       return <div className='inner-header'>
-                <div className='logo-img'>
-                    <img src={this.props.src}></img>
+                <div className='inner-header-logo'>        
+                    <div className='logo-img'>
+                        <img src={this.props.src}></img>
+                    </div>
+                    <div className='logo-text-img'>
+                        <img src={this.props.src2}></img>
+                    </div>
                 </div>
-                <div className='logo-text-img'>
-                    <img src={this.props.src2}></img>
+                <div className='inner-header-search'>
+                    <form role='search' method='get' className='search-form'>
+                        <label className='search-label'>Find a recipe:</label>
+                        <input type='search'></input>
+                    </form>
                 </div>
             </div>
     }
