6fc8f9e304b03335cd0605d3a568864d9dc04599
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-03 20:52:38 +0100

    Add SliderBox component

---
+++ src/components/SliderBox/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class SliderBox extends React.Component {
+    render() {
+      return <div className='slider-box'> 
+                <div className='slide-wrapper'>
+                  <div className='slide-author'> by Elise Bauer </div>
+                  <div className='slide-title'> Salsa Verde Chicken Enchiladas </div>
+                </div>
+            </div>
+    }
+  }
+ 
+export default SliderBox
