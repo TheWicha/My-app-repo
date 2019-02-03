a7b2b0654a72163fe56737124f6af92c5c45a3f3
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-03 20:52:56 +0100

    Add SliderBox component

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 
 class Slider extends React.Component {
     render() {
-      return <div className='slider' style={{backgroundImage: `url("${this.props.sliderImg}")`}}>
-            </div>
+      return <div className='slider' style={{backgroundImage: `url("${this.props.sliderImg}")`}} > {this.props.children} </div>
     }
   }
  
