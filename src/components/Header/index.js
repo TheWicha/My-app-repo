e9e15839f0700609d6a3afb9f364d09dabf391f8
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-03 11:06:15 +0100

    Break InnerHeader into smaller components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Navbar from '../Navbar';
 import NavLink from '../NavLink';
 import InnerHeader from '../InnerHeader';
-import img from '../../images/logo-mark.svg'
-import img2 from '../../images/logo-text.svg'
+import HeaderLogo from '../HeaderLogo';
+import HeaderSearch from '../HeaderSearch'
+import logoImg from '../../images/logo-mark.svg'
+import logoText from '../../images/logo-text.svg'
 
 
 
@@ -11,18 +13,17 @@ class Header extends React.Component {
     render() {
       return <div className='header'>
                 <Navbar>
-                        <NavLink href="#">Home</NavLink>
-                        <NavLink href="#">About</NavLink>
-                        <NavLink href="#">recipe index</NavLink>
-                        <NavLink href="#">recipe search</NavLink>
+                  <NavLink href="#">Home</NavLink>
+                  <NavLink href="#">About</NavLink>
+                  <NavLink href="#">recipe index</NavLink>
+                  <NavLink href="#">recipe search</NavLink>
                 </Navbar>
-                <InnerHeader src={img}
-                             src2={img2}
-                />
-
+                <InnerHeader>
+                  <HeaderLogo src={logoImg} src2={logoText} />
+                  <HeaderSearch />
+                </InnerHeader>
              </div>
     }
   }
  
 export default Header;
-
