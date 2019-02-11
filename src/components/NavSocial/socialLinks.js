59146e157dcd68dd8ab040ab64bc28bb95b1233c
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-11 14:24:03 +0100

    Move socialLinks images to seperate file

---
+++ src/components/NavSocial/socialLinks.js
@@ -0,0 +1,33 @@
+import '../../images/icon-facebook-gray.svg';
+import '../../images/icon-facebook-hover.svg';
+import '../../images/icon-pinterest-gray.svg';
+import '../../images/icon-pinterest-hover.svg';
+import '../../images/icon-ig-gray.svg';
+import '../../images/icon-ig-hover.svg';
+import '../../images/icon-snapchat-gray.svg';
+import '../../images/icon-snapchat-hover.svg';
+import '../../images/icon-twitter-gray.svg';
+import '../../images/icon-twitter-hover.svg';
+
+export default [
+    {
+        className: 'facebook',
+        link: '#',
+    }, 
+    {
+        className: 'pinterest',
+        link: '#',
+    },
+    {
+        className: 'instagram',
+        link: '#',
+    },
+    {
+        className: 'snapchat',
+        link: '#',
+    },
+    { 
+        className: 'twitter',
+        link: '#',
+    }
+]
