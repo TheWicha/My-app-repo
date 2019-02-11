ac01c5e24c6e03aa7a1ba8d63f9787a317976190
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-11 14:27:04 +0100

    Modify HeaderLogo images

--- src/components/HeaderLogo/index.js
+++ src/components/HeaderLogo/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-
+import '../../images/logo-mark.svg'
+import '../../images/logo-text.svg'
 
 class HeaderLogo extends React.Component {
     render() {
                return <div className='inner-header-logo'>        
                         <div className='logo-img'>
-                            <img src={this.props.src}></img>
+                            <img src="/images/logo-mark.svg"></img>
                         </div>
                         <div className='logo-text-img'>
-                            <img src={this.props.src2}></img>
+                            <img src="/images/logo-text.svg"></img>
                         </div>
                     </div>
     }
