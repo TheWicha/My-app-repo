d98ecc513e97714d854b8bb40da2f9c109d3bbde
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-18 22:20:11 +0100

    Add Routes for Navbar links

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import Header from './components/Header' 
-import PostList from './components/PostList';
+import Home from './components/Home';
+import Post from './components/Post';
+import About from './components/About';
 import Footer from './components/Footer';
-
+import { Route, BrowserRouter } from "react-router-dom";
 
 
 class Main  extends React.Component {
 
   render() {
-      return <div className="main-page-layout">
-                <Header />
-                <PostList />
-                <Footer />
-             </div>
+      return (
+      <BrowserRouter>
+        <div className="main-page-layout">
+        <Header />
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/recipe-index" component={Post} />
+            <Route path="/about" component={About} />
+        </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
+      )
     }
   }
  
