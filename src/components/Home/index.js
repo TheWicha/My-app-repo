f746f5cfc3d02474ce014b01a7d553d44f5f4910
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-18 22:13:22 +0100

    Add Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import PostTile from '../PostTile';
+import Slider from '../Slider'
+import LoadBox from './LoadBox';
+import ErrorBox from './ErrorBox';
+const url = 'http://localhost:8080/posts';
+
+class Home  extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [ {} ],
+      isLoading: false,
+      error: null,
+    }
+  }
+
+getPosts = async () => {
+    this.setState({ isLoading: true });
+
+    try {
+      const posts = await axios.get(url);
+
+      this.setState({
+        posts: posts.data, 
+        isLoading: false, 
+        error: null 
+      });
+    } catch(error) { 
+      this.setState({ 
+      error, 
+      isLoading: false 
+    })
+  }
+}
+
+componentDidMount() {
+    this.getPosts()
+ }
+
+  render() {
+    const { posts, isLoading, error } = this.state
+
+    if(error) {
+      return <ErrorBox 
+                error={error}
+                getPosts={this.getPosts}
+            />
+    }
+    
+    if(isLoading) {
+      return <LoadBox />
+    }
+
+    return (
+      <div>
+      <Slider />
+      <div className="post-list-wrapper">
+        <div className='post-list-header'>
+          <h3>Most Recent</h3>
+        </div>
+        <div className="post-list">
+          {
+            posts.map(post => 
+              <PostTile
+                id={post.id} 
+                author={post.author} 
+                title={post.title} 
+                content={post.content}
+              />
+            )
+          }
+        </div>  
+      </div>
+      </div>
+    )
+  }
+}
+ 
+export default Home;
