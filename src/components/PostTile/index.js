3ad2aa26e91d07fe8d3f25ab2b67877caf424090
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-21 23:33:34 +0100

    chage :id to :slug and add trimPostLength method

--- src/components/PostTile/index.js
+++ src/components/PostTile/index.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import '../../images/Valentines-Day-HERO.jpg'
+import { NavLink } from "react-router-dom";
 
 
 
 
 class PostTile extends React.Component {
-    
+
+    trimPostLength = post => {
+        const maxLength = 200;
+        const trimmingChars = '...'
+        const trimmedPost = (post.length > maxLength) ? post.substring(0, maxLength) : post
+        const trimmedWithNoSpace = (trimmedPost.slice(-1) == ' ') ? trimmedPost.replace(/\s*$/,'') : trimmedPost
+        
+        return `${trimmedWithNoSpace}${trimmingChars}`
+    }
+
     render() {
+
       return <div key={this.props.id} className="post-tile">
                 <div className='post-content-wrapper'>    
                     <div className="post-image">
-                        <a href="#">
-                            <img src='/images/Valentines-Day-HERO.jpg' />
-                        </a>
+                        <NavLink to={`/post/${this.props.slug}`}>
+                            <img src={this.props.thumbnail}/>
+                        </NavLink>
                     </div>
                     <a className ="post-author-link" href="#">
                         <div className="post-author">
@@ -20,16 +31,16 @@ class PostTile extends React.Component {
                         </div>
                     </a>
                     <h2 className="post-title-link">
-                        <a href="#">
+                        <NavLink to={`/post/${this.props.slug}`}>
                             <span>{this.props.title}</span>
-                        </a>
+                        </NavLink>
                     </h2>
                     <div className='post-content'>
-                        {this.props.content}
+                        {this.trimPostLength(this.props.content)}
                     </div>
-                    <a className="post-btn-link" href='#'>
+                    <NavLink className="post-btn-link" to={`/post/${this.props.slug}`}>
                         <button className="post-btn">Get the recipe</button>
-                    </a>
+                    </NavLink>
                 </div>    
              </div>
     }
