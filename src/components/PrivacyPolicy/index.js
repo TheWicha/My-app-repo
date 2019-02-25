c48e4f3ad98ada3af0dcf283a0e94c89c02e541d
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-02-25 19:58:08 +0100

    add Advertise and PrivatePolicy Components

---
+++ src/components/PrivacyPolicy/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class PrivacyPolicy extends Component {
+  render() {
+    return (
+      <div className="post">
+        <h1>Privacy Policy</h1>
+            <div className="entry">
+                <p>
+                MY-APP Recipes is committed to maintaining the privacy and protection of your personal information. 
+                This privacy policy tells you about the information we collect on our website located at www.MY-APPrecipes.com (“Site”) 
+                and how we use that information. By using the Site, you are consenting to the information collection and use practices described in 
+                this privacy policy, which may be modified from time to time by us.
+                </p>
+                <h4>Children Under 13</h4>
+                <p>
+                We care about protecting the privacy of children. We won’t knowingly allow anyone under 13 to provide us any personally
+                identifying information. Children should always get permission from their parents before sending any information about themselves
+                (such as their names, email addresses, and phone numbers) over the Internet, to us or to anyone else. If you’re under 13, please 
+                do not provide us with any personally identifying information (such as your name, your email address or your phone number).
+                </p>
+            </div>
+      </div>
+    );
+  }
+}
+
+export default PrivacyPolicy;
