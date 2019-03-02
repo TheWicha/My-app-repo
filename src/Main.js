bf128fbcd652f881e5c39943203696368e74187c
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-03-02 10:33:34 +0100

    Move routed to Main component, Use layoutHoc for Route

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import Header from './components/Header' 
-import Footer from './components/Footer';
-import { Route, BrowserRouter } from "react-router-dom";
-import Content from './components/Content';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './components/Home';
+import Recipe from './components/Recipe';
+import About from './components/About';
+import Post from './components/Post'
+import Advertise from './components/Advertise'
+import PrivacyPolicy from './components/PrivacyPolicy';
+import layoutHoc from './components/Layout'
 
 
 class Main  extends React.Component {
 
-  render() {
+  render() { 
       return (
-      <BrowserRouter>
-        <div className="main-page-layout">
-          <Header />
-          <Content />
-          <Footer />
-        </div>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Route exact path="/"  component={layoutHoc(Home)} />
+          <Route path="/recipe-index" component={layoutHoc(Recipe)} />
+          <Route path="/about" component={layoutHoc(About)} />
+          <Route path="/post/:slug" component={layoutHoc(Post)} />
+          <Route path="/advertise" component={layoutHoc(Advertise)} />
+          <Route path="/privacy-policy" component={layoutHoc(PrivacyPolicy)} />
+        </BrowserRouter>
       )
     }
   }
